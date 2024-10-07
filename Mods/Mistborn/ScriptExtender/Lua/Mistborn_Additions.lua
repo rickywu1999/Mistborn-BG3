55fc866b48d7e4f839af25aef9607371eb9139a2
Ricky Wu <rickywu1999@gmail.com>
2024-10-07 03:50:11 -0700

    Mental Allomancer subclass

--- Mods/Mistborn/ScriptExtender/Lua/Mistborn_Additions.lua
+++ Mods/Mistborn/ScriptExtender/Lua/Mistborn_Additions.lua
@@ -10,7 +10,7 @@ Ext.Osiris.RegisterListener("UsingSpellOnTarget", 6, "after", function(_, target
     end
 end)
 
-Ext.Osiris.RegisterListener("StatusApplied", 4, "after", function(object, status, _,_)
+Ext.Osiris.RegisterListener("StatusApplied", 4, "after", function(object, status, causee,_)
     if status == "IRONREDIRECTION_ACTIVATED" then
         Osi.UseSpell(object, PersistentVars['IronRedirectionSpellBlocked'],PersistentVars['IronRedirectionAuraOwner'])
         Osi.RemoveStatus(object,"IRONREDIRECTION_ACTIVATED","")
@@ -20,6 +20,92 @@ Ext.Osiris.RegisterListener("StatusApplied", 4, "after", function(object, status
             PersistentVars['IronRedirectionAuraOwner'] = nil
         end
         PersistentVars['IronRedirectionAuraOwner']=object
+    end
+    if (status == "CHARMED_BRASS" or status == "MENTAL_DURALUMIN_EMOTIONLESS" or status == "FRIGHTENED_ZINC" or status == "FEARED_ZINC") and (Osi.HasPassive(causee,'Class_Feature_Spreading_Hysteria')==1) then
+        local smallest1,smallest2,smallest3 = math.huge,math.huge,math.huge
+        local smallestob1,smallestob2,smallestob3 = "","",""
+        for k, v in pairs(Osi.DB_Is_InCombat:Get(nil, Osi.CombatGetGuidFor(Ext.Entity.Get(object).Uuid.EntityUuid))) do
+            if(IsEnemy(v[1],causee) == 1 and v[1] ~= object) then
+                local distance = Osi.GetDistanceTo(object, v[1])
+                if (distance <= 10) then
+                    if distance < smallest1 then
+                        smallest3 = smallest2
+                        smallest2 = smallest1
+                        smallest1 = distance
+                        smallestob3 = smallestob2
+                        smallestob2 = smallestob1
+                        smallestob1 = v[1]
+                    elseif distance < smallest2 then
+                        smallest3 = smallest2
+                        smallest2 = distance
+                        smallestob3 = smallestob2
+                        smallestob2 = v[1]
+                    elseif distance < smallest3 then
+                        smallest3 = distance
+                        smallestob3 = v[1]
+                    end
+                end
+            end
+        end
+        if (Osi.HasPassive(causee,'Class_Feature_Mass_Hysteria')==1) then
+            if (status == "CHARMED_BRASS") then
+                if (smallestob3 ~= "") then
+                    Osi.ApplyStatus(smallestob3,"CHARMED_BRASS_ATTEMPT",1,100,causee)
+                end
+                if (smallestob2 ~= "") then
+                    Osi.ApplyStatus(smallestob2,"CHARMED_BRASS_ATTEMPT",1,100,causee)
+                end
+                if (smallestob1 ~= "") then
+                    Osi.ApplyStatus(smallestob1,"CHARMED_BRASS_ATTEMPT",1,100,causee)
+                end
+            end
+            if (status == "MENTAL_DURALUMIN_EMOTIONLESS") then
+                if (smallestob3 ~= "") then
+                    Osi.ApplyStatus(smallestob3,"MENTAL_DURALUMIN_EMOTIONLESS_ATTEMPT",1,100,causee)
+                end
+                if (smallestob2 ~= "") then
+                    Osi.ApplyStatus(smallestob2,"MENTAL_DURALUMIN_EMOTIONLESS_ATTEMPT",1,100,causee)
+                end
+                if (smallestob1 ~= "") then
+                    Osi.ApplyStatus(smallestob1,"MENTAL_DURALUMIN_EMOTIONLESS_ATTEMPT",1,100,causee)
+                end
+            end
+            if (status == "FRIGHTENED_ZINC") then
+                if (smallestob3 ~= "") then
+                    Osi.ApplyStatus(smallestob3,"FRIGHTENED_ZINC_ATTEMPT",1,100,causee)
+                end
+                if (smallestob2 ~= "") then
+                    Osi.ApplyStatus(smallestob2,"FRIGHTENED_ZINC_ATTEMPT",1,100,causee)
+                end
+                if (smallestob1 ~= "") then
+                    Osi.ApplyStatus(smallestob1,"FRIGHTENED_ZINC_ATTEMPT",1,100,causee)
+                end
+            end
+            if (status == "FEARED_ZINC") then
+                if (smallestob3 ~= "") then
+                    Osi.ApplyStatus(smallestob3,"FEARED_ZINC_ATTEMPT",1,100,causee)
+                end
+                if (smallestob2 ~= "") then
+                    Osi.ApplyStatus(smallestob2,"FEARED_ZINC_ATTEMPT",1,100,causee)
+                end
+                if (smallestob1 ~= "") then
+                    Osi.ApplyStatus(smallestob1,"FEARED_ZINC_ATTEMPT",1,100,causee)
+                end
+            end
+        elseif (Osi.HasPassive(causee,'Class_Feature_Spreading_Hysteria')==1) then
+            if (status == "CHARMED_BRASS" and smallestob1 ~= "") then
+                Osi.ApplyStatus(smallestob1,"CHARMED_BRASS_ATTEMPT",1,100,causee)
+            end
+            if (status == "MENTAL_DURALUMIN_EMOTIONLESS" and smallestob1 ~= "") then
+                Osi.ApplyStatus(smallestob1,"MENTAL_DURALUMIN_EMOTIONLESS_ATTEMPT",1,100,causee)
+            end
+            if (status == "FRIGHTENED_ZINC" and smallestob1 ~= "") then
+                Osi.ApplyStatus(smallestob1,"FRIGHTENED_ZINC_ATTEMPT",1,100,causee)
+            end
+            if (status == "FEARED_ZINC" and smallestob1 ~= "") then
+                Osi.ApplyStatus(smallestob1,"FEARED_ZINC_ATTEMPT",1,100,causee)
+            end
+        end
     end
 end)
 
